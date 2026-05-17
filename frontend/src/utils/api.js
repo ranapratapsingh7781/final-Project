55b3605ea7894332b8933ac65578d895feb142c6
ranapratapsingh7781 <ranapratapsingh7781@gmail.com>
2026-05-18 00:50:01 +0530

    fix deployment

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5002/api',
+  baseURL: process.env.REACT_APP_API_URL || (process.env.NODE_ENV === 'development' ? 'http://localhost:5002/api' : '/api'),
   headers: {
     'Content-Type': 'application/json',
   },
